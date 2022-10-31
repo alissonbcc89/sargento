b91009561a1eb2eede74f2ebc2065b426cc3d525
Alisson Cavalcanti <ajsc@ibm.com>
2022-10-30 21:59:16 -0300

    Tentando introduzir um ponteiro para marca pavimento do heroi

--- src/GravityBoy.java
+++ src/GravityBoy.java
@@ -18,26 +18,30 @@ public class GravityBoy {
         //String[] piso = {"Inferior", "Superior"};
        // enum pis { Superior, Terreo; }
         Setor local = Setor.Piso;
-        int p = 0;
+        //int p = 0;
 
         for (int i = 0; i < n; i++) {
-            if (local.equals(Setor.Piso) && piso[i] == 0) {
-                local = Setor.Teto;
-                gravidade = teto[i];
-                break;
+            if(i == 0) {
+                if (piso[i] == 0) {
+                    local = Setor.Teto;
+                    gravidade = teto[i];
+                    break;
+                }
             }
-            if (local.equals(Setor.Piso) && piso[i] != 0) {
-                local = Setor.Piso;
-                gravidade = 1;
-                break;
-            }
-            if (local.equals(Setor.Teto) && teto[i] > gravidade) {
-                gravidade = piso[i];
+            if(i>0) {
+                if (local.equals(Setor.Piso) && piso[i] == 0) {
+                    local = Setor.Teto;
+                    gravidade = teto[i - 1];
+                    break;
+                }
+                if (local.equals(Setor.Teto) && teto[i] > gravidade) {
+                    gravidade = piso[i];
 
-            }
-            if (local.equals(Setor.Teto) && teto[i] == 0) {
-                local = Setor.Piso;
+                }
+                if (local.equals(Setor.Teto) && teto[i] == 0) {
+                    local = Setor.Piso;
 
+                }
             }
         }
         return gravidade;
@@ -46,18 +50,26 @@ public class GravityBoy {
 
     public static void main(String[] args) throws IOException {
 
+        //5
+        //
+        //1 1 0 1 1
+        //
+        //1 2 2 2 1
        int n = 5;
 
-        int[] teto = {0,2,1,1,0};
+        int[] teto = {1,1,0,1,1};
+        int[] piso = {1,2,2,2,1};
+       // int[] teto = {0,2,1,1,0};
 
-        int[] piso = {1,1,0,1,1};
+        //int[] piso = {1,1,0,1,1};
 
         GravityBoy gravityBoy = new GravityBoy();
 
-        gravityBoy.inicio(n,teto,piso);
+        System.out.print(gravityBoy.inicio(n,teto,piso));
 
     }
 }
+
 
 
 
